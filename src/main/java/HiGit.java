e128bb058ce58afb1ec244fbba932108a218afd4
ccc <793117343@qq.com>
2021-07-06 14:24:08 +0800

    更新1

--- src/main/java/HiGit.java
+++ src/main/java/HiGit.java
@@ -2,5 +2,7 @@ public class HiGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("123");
+        System.out.println("更新1");
+
     }
 }
